5bdbdbb95da90b04dd43e120c47e3d58f6cea80e
Leonardo <leoaranguren10@gmail.com>
2019-04-21 19:11:03 -0400

    adding files

---
+++ src/components/Particles/Particles.js
@@ -0,0 +1,109 @@
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 90,
+      density: {
+        enable: true,
+        value_area: 900
+      }
+    },
+    color: {
+      value: "#ffffff"
+    },
+    shape: {
+      type: "circle",
+      stroke: {
+        width: 0,
+        color: "#000000"
+      },
+      polygon: {
+        nb_sides: 5
+      },
+     
+    },
+    opacity: {
+      value: 0.5,
+      random: false,
+      anim: {
+        enable: false,
+        speed: 2,
+        opacity_min: 0.1,
+        sync: false
+      }
+    },
+    size: {
+      value: 3,
+      random: true,
+      anim: {
+        enable: false,
+        speed: 30,
+        size_min: 0.1,
+        sync: false
+      }
+    },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: "#ffffff",
+      opacity: 0.4,
+      width: 1
+    },
+    move: {
+      enable: true,
+      speed: 6,
+      direction: "none",
+      random: false,
+      straight: false,
+      out_mode: "out",
+      bounce: false,
+      attract: {
+        enable: false,
+        rotateX: 600,
+        rotateY: 1200
+      }
+    }
+  },
+  interactivity: {
+    detect_on: "window",
+    events: {
+      onhover: {
+        enable: true,
+        mode: "repulse"
+      },
+      onclick: {
+        enable: true,
+        mode: "bubble"
+      },
+      resize: true
+    },
+    modes: {
+      grab: {
+        distance: 400,
+        line_linked: {
+          opacity: 1
+        }
+      },
+      bubble: {
+        distance: 200,
+        size: 20,
+        duration: 0.5,
+        opacity: 5,
+        speed: 3
+      },
+      repulse: {
+        distance: 150,
+        duration: 0.4
+      },
+      push: {
+        particles_nb: 4
+      },
+      remove: {
+        particles_nb: 2
+      }
+    }
+  },
+  retina_detect: true
+}
+
+export default particlesOptions;
---
+++ src/components/Register/Register.js
@@ -0,0 +1,122 @@
+import React from 'react';
+
+class Register extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      name: ''
+    }
+  }
+
+  onNameChange = (event) => {
+    let key = event.KeyCode || event.which;
+    let tecla = String.fromCharCode(key).toLowerCase();
+    let letters = "áéíóúabcdefghijklmnñopqrstuvwxyz";
+    this.setState({ name: event.target.value}, () => {
+      console.log("name", this.state.name)
+  });
+    if (letters.indexOf(tecla) === -1) {
+      alert("ONLY ENTER LETTERS");
+      event.preventDefault();
+      return false;
+    }
+   
+    console.log("key:", event.key); 
+  }
+
+  onEmailChange = (event) => {
+    this.setState({email: event.target.value})
+    let text = event.target.value;
+    
+    if (text.includes("@gmail.com") || text.includes("@hotmail.com")) {
+      document.getElementById("email-address").style = "hover-bg-black";
+      document.getElementById("notvalid").style.display = "none";
+    }
+    else {
+      document.getElementById("email-address").style.backgroundColor = "red"; 
+      document.getElementById("notvalid").style.display = "block";
+    }
+  }
+
+  onPasswordChange = (event) => {
+    this.setState({password: event.target.value})
+  }
+
+  onSubmitSignIn = () => {
+    fetch('https://rocky-hollows-20234.herokuapp.com/register', {
+      method: 'post',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        email: this.state.email,
+        password: this.state.password,
+        name: this.state.name
+      })
+    })
+      .then(response => response.json())
+      .then(user => {
+        if (user.id) {
+          this.props.loadUser(user)
+          this.props.onRouteChange('home');
+        }
+      })
+  }
+
+  render() {
+    return (
+      <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
+        <main className="pa4 black-80">
+          <div className="measure">
+            <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+              <legend className="f1 fw6 ph0 mh0">Register</legend>
+              <div className="mt3">
+                <label className="db fw6 lh-copy f6" htmlFor="name">Name</label>
+                <input
+                  className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                  type="text"
+                  name="name"
+                  id="name"
+                  onKeyPress={this.onNameChange}
+                />
+              </div>
+              <div className="mt3">
+                <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
+                <input
+                  className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                  type="email"
+                  name="email-address"
+                  id="email-address"
+                  onChange={this.onEmailChange}
+                />
+                
+                <label id="notvalid" className="db fw6 lh-copy f6" htmlFor="email-address">Enter @gmail.com or @hotmail.com</label>
+              </div>
+              <div className="mv3">
+                <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
+                <input
+                  className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                  type="password"
+                  name="password"
+                  id="password"
+                  onChange={this.onPasswordChange}
+                />
+              </div>
+            </fieldset>
+            <div className="">
+              <input
+                className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
+                type="submit"
+                value="Register"
+                id="register"
+                onClick={this.onSubmitSignIn}
+              />
+            </div>
+          </div>
+        </main>
+      </article>
+    );
+  }
+}
+
+export default Register;
